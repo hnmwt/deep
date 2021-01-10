bb1590282912677f18333a38008d3c8545e57b90
Your Name <you@example.com>
2021-01-10 09:47:46 +0900

    2021011009

--- FX/oanda/oandaAPI_main.py
+++ FX/oanda/oandaAPI_main.py
@@ -5,11 +5,12 @@ import json
 from oandapyV20 import API
 import oandapyV20.endpoints.instruments as instruments
 import oandapyV20.endpoints.accounts as accounts
+from oandapyV20.endpoints.pricing import PricingStream
 #numpy配列を省略しないようにする
 np.set_printoptions(threshold=np.inf)
 
 access_token_production = 'ff123428dffcc26c50a1991605df24b7-85946fa231df46d2ac24bf30e1e424b2'
-accountID = '	101-009-17604085-001'
+accountID = '101-009-17604085-001'
 access_token = '8f700c7d32cfa05988fede188bac48f0-72b3bcf05a1a02fbd487f05dc22e6a6e'  # デモ口座
 api = API(access_token=access_token, environment='practice')
 
@@ -33,10 +34,27 @@ def json_to_csv(json, name):
     df = json_normalize(json)
     df.to_csv(name , encoding='shift_jis')
 
+#  為替情報取得
 instruments_candles = instruments.InstrumentsCandles(instrument=instrument, params=params)  # 為替情報取得
 response = api_response(instruments_candles)  # レスポンス送信
 json_to_csv(response['candles'], '.\output\param.csv')  # csv化
 
+#  アカウント情報取得
 account_summary = accounts.AccountSummary(accountID)  # アカウント情報取得
 response = api_response(account_summary)  # レスポンス送信
 json_to_csv(response,'.\output\AccountSummary.csv')  # csv化
+
+# リアルタイムレート取得
+pricing_stream = PricingStream(accountID, params)
+response = api_response(account_summary)  # レスポンス送信
+json_to_csv(response,'.\output\pricing_stream.csv')  # csv化
+
+# オープンポジション取得
+instruments_order_book = instruments.InstrumentsOrderBook(instrument=instrument,params=params)
+response = api_response(account_summary)  # レスポンス送信
+json_to_csv(response,'.\output\instruments_order_book.csv')  # csv化
+
+# オーダーポジション取得
+instruments_position_book = instruments.InstrumentsPositionBook(instrument=instrument,params=params)
+response = api_response(account_summary)  # レスポンス送信
+json_to_csv(response,'.\output\instruments_position_book.csv')  # csv化
